1b1015fa3b23e3dc10837ac58c7f123219d5578f
Mario Ocampo <djsonicomendoza@gmail.com>
2025-04-17 11:06:51 -0300

    reinicio

--- src/firebase/firebaseAdmin.js
+++
@@ -1,9 +0,0 @@
-// backend/firebaseAdmin.js
-const admin = require('firebase-admin');
-const serviceAccount = require('./config/firebaseServiceAccount.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
